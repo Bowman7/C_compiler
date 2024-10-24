c85a2bebc3a61fd5d7d22c89b75d1ecd7e784f03
sulayen_ <110030682+Bowman7@users.noreply.github.com>
2024-10-24 20:33:33 +0530

    wip

--- first.c
+++ first.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 
-#define MAX_VARIABLES 3
+#define MAX_VARIABLES 2
 #define MAX_PRODUCTION 100
 
 void Find_FirstSet(char* production,char* first_set);
@@ -26,27 +26,80 @@ int main(){
   //find first set
   Find_FirstSet(production[0],first_set[0]);
 
-  printf("\nInt main: %c \n",production[0][0]);
+  //printf("\nInt main: %c \n",production[0][0]);
 
   //print first set
   printf("First set: \n");
   for(int i=0;i<MAX_VARIABLES;i++){
-    printf("string : %c\n",first_set[i][0]);
+    printf("string : %s\n",first_set[i]);
   }
   return 0;
 }
 
 void Find_FirstSet(char* production,char* first_set){
-  printf("first of prod 0: %c",production[0]);
+  //set variables 
+  char variables[MAX_VARIABLES];
+  for(int i=0;i<MAX_VARIABLES;i++){
+    char temp = production[i*MAX_PRODUCTION];
+    variables[i] = temp;
+  }
+  //print variables
+   for(int i=0;i<MAX_VARIABLES;i++){
+     printf("var :%c\n",variables[i]);
+  }
 
   for(int i=0;i<MAX_VARIABLES;i++){
-    //printf("\nfirst of %d: %c",i,production[i*MAX_PRODUCTION]);
-    char val;
-    val = production[i*MAX_PRODUCTION];
-    first_set[i*MAX_PRODUCTION] = val;
-  }
-  //chage first val
-  production[0] = 'X';
+    int count=0;
+    int k=2;
+    while(production[i*MAX_PRODUCTION+k]!='\0'){
+      k++;
+      count++;
+    }
+    printf("Number of val in rhs: %d \n",count);
 
-  
+    //iterate the production
+
+    char val;
+    int f_pval = 0;
+    val = production[i*MAX_PRODUCTION+2];
+    first_set[i*MAX_PRODUCTION+f_pval] = val;
+    f_pval++;
+
+    for(int j=3;j<=count;j++){
+      char val_cur = production[i*MAX_PRODUCTION+j];
+      if(val_cur == '|'){
+	char temp = production[i*MAX_PRODUCTION+j+1];
+	first_set[i*MAX_PRODUCTION+f_pval] = temp;
+	f_pval++;
+      }
+    }
+  }
+  //swap the var with teminals
+  for(int i=0;i<MAX_VARIABLES;i++){
+    int count =0;
+    while(first_set[i*MAX_PRODUCTION+count]!='\0'){
+      count++;
+    }
+
+    //now check if it is a var
+    for(int k=0;k<count;k++){
+      char val = first_set[i*MAX_PRODUCTION+k];
+      printf("val first : %c\n",val);
+      for(int i=0;i<MAX_VARIABLES;i++){
+	if(variables[i] == val){
+	  printf("It is a variable!\n");
+	  //if variables push its first set val into the first set pos
+	  printf("its first set are : \n");
+	  printf("val :%c\n",first_set[i*MAX_PRODUCTION]);
+	  //count val in a new char*
+	  char* newSet;
+	  
+	}
+      }
+    }
+    printf("Num of prod in first: %d\n",count);
+  }
+
+  printf("\n");
 }
+
